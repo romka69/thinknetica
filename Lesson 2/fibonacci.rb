8a9c6c12adcf02c6f08994780b74a5bf2c5642a6
roman <romka69tver@gmail.com>
2018-11-08 22:55:05 +0300

    now without variables

--- Lesson 2/fibonacci.rb
+++ Lesson 2/fibonacci.rb
@@ -1,14 +1,11 @@
 puts "Скрипт выводит в массиве ряд фибоначчи до 100."
 
-fib1, fib2 = 1, 1
 fib_array = [1, 1]
 
 loop do
-  fib_sum = fib2 + fib1
+  fib_sum = fib_array.last(2).sum
   break if fib_sum > 100
   fib_array.push(fib_sum)
-  fib1 = fib2
-  fib2 = fib_sum
 end
 
 print fib_array
