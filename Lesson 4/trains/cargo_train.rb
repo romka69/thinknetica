0ba81c2d07b0b3f4bdcbfebb5bf0cb8605b8dc80
roman <romka69tver@gmail.com>
2018-11-13 00:17:48 +0300

    adds type train classes

---
+++ Lesson 4/trains/cargo_train.rb
@@ -0,0 +1,5 @@
+class CargoTrain < Train
+  def initialize(number, type = :cargo, wagons)
+    super
+  end
+end
---
+++ Lesson 4/trains/passenger_train.rb
@@ -0,0 +1,5 @@
+class PassengerTrain < Train
+  def initialize(number, type = :passenger, wagons)
+    super
+  end
+end
